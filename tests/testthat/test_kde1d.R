e4142b6dc4fd9fcafe92e00097fccdaeeaf1d42b
tvatter <thibault.vatter@gmail.com>
2018-03-21 13:16:28 -0400

    improve tests for coverage

--- tests/testthat/test_kde1d.R
+++ tests/testthat/test_kde1d.R
@@ -1,21 +1,40 @@
 context("Testing 'kde1d'")
 
 set.seed(0)
-fits <- lapply(c("unbounded", "bounded", "discrete"),
+n_sim <- 1e2
+fits <- lapply(c("unbounded", "left_boundary", "right_boundary",
+                 "two_boundaries", "discrete"),
                function(type) {
                    xmin <- xmax <- NaN
                    if (type == "unbounded") {
-                       x <- rnorm(1e2)
-                   } else if (type == "bounded") {
-                       x <- runif(1e2)
+                       x <- rnorm(n_sim)
+                   } else if (type == "left_boundary") {
+                       x <- rexp(n_sim)
+                       xmin <- 0
+                   }  else if (type == "right_boundary") {
+                       x <- -rexp(n_sim)
+                       xmax <- 0
+                   }  else if (type == "two_boundaries") {
+                       x <- runif(n_sim)
                        xmin <- 0
                        xmax <- 1
                    } else {
-                       x <- ordered(rbinom(1e2, size = 5, prob = 0.5),
+                       x <- ordered(rbinom(n_sim, size = 5, prob = 0.5),
                                     levels = 0:5)
                    }
                    return(kde1d(x, xmin = xmin, xmax = xmax))
                })
+
+test_that("detects wrong arguments", {
+    x <- rnorm(n_sim)
+    expect_error(kde1d(x, xmin = 0))
+    expect_error(kde1d(x, xmax = 0))
+    expect_error(kde1d(x, xmin = 10, xmax = -10))
+    expect_error(kde1d(x, mult = 0))
+
+    x <- ordered(rbinom(n_sim, size = 5, prob = 0.5), levels = 0:5)
+    expect_error(kde1d(x, xmax = 0))
+})
 
 test_that("returns proper 'kde1d' object", {
     lapply(fits, function(x) expect_s3_class(x, "kde1d"))
@@ -29,6 +48,7 @@ test_that("d/p/r/h functions work", {
     n <- 50
     u <- runif(n)
     test_dpqr <- function(fit, sim) {
+        sim <- data.frame(sim)
         is_jittered <- length(fit$jitter_info$i_disc) == 1
         if (is.nan(fit$xmax)) {
             xmax <- ifelse(is_jittered, fit$jitter_info$nu, Inf)
@@ -70,14 +90,16 @@ test_that("plot functions work", {
     lapply(fits, test_plot)
 })
 
-test_that("print/summary generics work", {
+test_that("other generics work", {
 
-    test_print_summary <- function(fit) {
+    test_other_generics <- function(fit) {
         expect_output(print(fit))
         expect_output(s <- summary(fit))
         expect_is(s, "numeric")
         expect_equal(length(s), 4)
+        expect_silent(s <- logLik(fit))
+        expect_is(s, "numeric")
     }
 
-    lapply(fits, test_print_summary)
+    lapply(fits, test_other_generics)
 })
