5b495a1b39e19ba577c47937d570591f3b5fad5e
tnagler <thomas.nagler@tum.de>
2019-11-14 16:44:16 +0100

    almost done with fft

--- inst/include/kde1d/kdefft.hpp
+++ inst/include/kde1d/kdefft.hpp
@@ -40,6 +40,7 @@ private:
 inline KdeFFT::KdeFFT(const Eigen::VectorXd& x,
                       double bw,
                       const Eigen::VectorXd& weights)
+                      : bw_(bw)
 {
   if (weights.size() > 0 && (weights.size() != x.size()))
     throw std::runtime_error("x and weights must have the same size");
@@ -51,8 +52,8 @@ inline KdeFFT::KdeFFT(const Eigen::VectorXd& x,
     w = Eigen::VectorXd::Ones(x.size());
   }
 
-  lower_ = x.minCoeff() - 3 * bw;
-  upper_ = x.maxCoeff() + 3 * bw;
+  lower_ = x.minCoeff() - 4 * bw;
+  upper_ = x.maxCoeff() + 4 * bw;
   bin_counts_ = linbin(x, lower_, upper_, w);
 }
 
@@ -91,14 +92,14 @@ KdeFFT::kde_drv(size_t drv) const
   double delta = (upper_ - lower_) / num_bins_;
   double tau = 4.0 + drv;
   size_t L = std::floor(tau * bw_ / delta);
-  L = std::min(L, num_bins_);
+  L = std::min(L, num_bins_ + 1);
 
   double tmp_dbl = L * delta / bw_;
   Eigen::VectorXd arg = Eigen::VectorXd::LinSpaced(L + 1, 0.0, tmp_dbl);
   tmp_dbl = std::pow(bw_, drv + 1.0);
   arg = stats::dnorm_drv(arg, drv) / (tmp_dbl * bin_counts_.sum());
 
-  tmp_dbl = num_bins_ + L + 1.0;
+  tmp_dbl = num_bins_ + L + 2.0;
   tmp_dbl = std::pow(2, std::ceil(std::log(tmp_dbl) / std::log(2)));
   size_t P = static_cast<size_t>(tmp_dbl);
 
@@ -107,14 +108,14 @@ KdeFFT::kde_drv(size_t drv) const
   arg2.tail(L) = arg.tail(L).reverse() * (drv % 2 ? -1.0 : 1.0);
 
   Eigen::VectorXd x2 = Eigen::VectorXd::Zero(P);
-  x2.head(num_bins_) = bin_counts_;
+  x2.head(num_bins_ + 1) = bin_counts_;
 
   Eigen::FFT<double> fft;
   Eigen::VectorXcd tmp1 = fft.fwd(arg2);
   Eigen::VectorXcd tmp2 = fft.fwd(x2);
   tmp1 = tmp1.cwiseProduct(tmp2);
   tmp2 = fft.inv(tmp1);
-  return tmp2.head(num_bins_).real();
+  return tmp2.head(num_bins_ + 1).real();
 }
 
 } // end kde1d::bw
