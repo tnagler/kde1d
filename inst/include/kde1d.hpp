3d6a32139c8bd3ec89e9fd9f60f2eab517d5861c
tnagler <thomas.nagler@tum.de>
2026-08-22 16:18:26 +0200

    Use kde1d-cpp as a pinned submodule

--- inst/include/kde1d.hpp
+++ inst/include/kde1d.hpp
@@ -1,3 +1,3 @@
 #pragma once
 
-#include "kde1d/kde1d.hpp"
+#include <kde1d-cpp/include/kde1d.hpp>
--- src/update_kde1d-cpp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone --depth 1 git@github.com:vinecopulib/kde1d-cpp.git -b main  --single-branch
-
-rm -rf ../inst/include/kde1d/*
-mv ./kde1d-cpp/include/* ../inst/include
-
-rm -rf ./../inst/include/kde1d/mainpage.h
-rm -rf kde1d-cpp
